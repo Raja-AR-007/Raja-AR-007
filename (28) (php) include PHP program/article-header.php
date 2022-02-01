7e86c79ea5f732ab26803a30a66a51478db6f36a
Raja-AR-007 <92211139+Raja-AR-007@users.noreply.github.com>
2022-02-01 08:54:25 +0530

    Add files via upload

---
+++ (28) (php) include PHP program/article-header.php
@@ -0,0 +1,3 @@
+<h1><?php echo $title;  ?><h1>
+<h3><?php echo $author;  ?><h3>
+<h4><?php echo $words;  ?><h4>
---
+++ (28) (php) include PHP program/include-PHP.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+                    <!--      include PHP (3:36:51)            -->
+                   <!--   include "article-header.php"    -->
+   <!--
+    <?php
+/*
+    $title="Raja Stories";
+    $author="Raj";
+    $words=500;
+    include "article-header.php";  */
+
+    ?>  -->
+
+    <!--
+    output:
+    Raja Stories
+    Raj
+    500
+
+    -->
+
+                   <!--   include "function.php"    -->
+
+    <?php
+
+    include "function.php";
+    sayHi("Raja<br>");
+    echo $releaseYear;
+    ?>
+
+    <!--
+    output:
+    Hello Raja
+    1999 -->
+
+  </body>
+</html>
