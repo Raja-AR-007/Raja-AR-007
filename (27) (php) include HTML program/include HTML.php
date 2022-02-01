7a8b8976a54d7cdbd55bf30051f5a48c77ac4c33
Raja-AR-007 <92211139+Raja-AR-007@users.noreply.github.com>
2022-02-01 08:53:19 +0530

    Add files via upload

---
+++ (27) (php) include HTML program/include HTML.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+                     <!--  include HTML (3:31:08)    -->
+    <?php include "header.html" ?>
+    <P><h5>Hello world<h5><p>
+    <?php include "footer.html" ?>
+
+  </body>
+</html>
